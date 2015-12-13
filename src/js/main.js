60615dfc384981f33ec25537e67e411abb892e89
ericquayson <ericquayson@github.com>
2015-12-13 16:50:51 -0500

    routing the page

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,25 @@
 
   angular.module('ncpride', ['ngRoute'], function($routeProvider) {
     $routeProvider
-    .when('/', {
+    .when('/home', {
       redirectTo: '/landing'
     })
     .when('/landing', {
       templateUrl: 'partials/landing.html',
     })
-  });
+    .when('/History', {
+      templateUrl: 'partials/history.html',
+    })
+    .when('/Nightfest', {
+      templateUrl: 'partials/nightfest.html',
+    })
+    .when('/pride', {
+      templateUrl: 'partials/pride2015.html',
+    })
+    .when('/Apparel', {
+      templateUrl: 'partials/apparel.html',
+    });
 
-
+  })
 
 })();
